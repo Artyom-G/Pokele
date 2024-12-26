0245cc8c7c0e9252c80e209126efddf577098a9f
Artyom WSL <artyomgv05@gmail.com>
2024-12-25 20:45:48 -0500

    backend finished????

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ def get_all_pokemon():
 def get_pokemon(id):
     pokemon = Pokemon.query.get(id)
     if not pokemon:
-        return (jsonify({"message": "Pokemon not found"}), 404)
+        return jsonify({"message": "Pokemon not found"}), 404
     return jsonify({"pokemon": pokemon.to_json()}), 200
 
 @app.route("/api/v1/today", methods=["GET"])
@@ -29,6 +29,14 @@ def get_today_pokemon():
     id = random.randint(0, modulo)+1
     print(f"Generating with seed {seed}, returns #{id}")
     return get_pokemon(id)
+
+@app.route("/api/v1/validate/<int:id>", methods=["GET"])
+def validate_pokemon(id):
+    pokemon_today, status_today = get_today_pokemon()
+    pokemon_id, status_id = get_pokemon(id)
+    if(status_today != 200 or status_id != 200): return jsonify({"message": "Error!"}), 404
+    print("Checking ", json.loads(pokemon_today.data)["pokemon"]["name"], " and ", json.loads(pokemon_id.data)["pokemon"]["name"])
+    return jsonify({"response": json.loads(pokemon_today.data)["pokemon"]["name"] == json.loads(pokemon_id.data)["pokemon"]["name"]}), 200
 
 if __name__ == "__main__":
     with app.app_context():
